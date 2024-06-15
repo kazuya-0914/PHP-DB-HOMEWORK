3322f95af67b143cee810c7f81ddcee4ee88422c
kazuya-0914 <100980325+kazuya-0914@users.noreply.github.com>
2024-06-15 17:08:06 +0900

    初回コミット

---
+++ js/script.js
@@ -0,0 +1,19 @@
+const areaBtn = document.getElementById('area-btn');
+const infoBtn = document.getElementById('info-btn');
+
+/*
+const weatherInfo = () => {
+  const url = `https://api.openweathermap.org/data/2.5/weather?q=${cityName}&appid=${appKey}`;
+  fetch(url)
+    .then((res) => res.json())
+    .then((data) => {
+
+    });
+};
+
+infoBtn.addEventListener('click', ()=> {
+  const infoData = [
+    
+  ]
+});
+*/
